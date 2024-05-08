53c84601527a4b5fb52d67a612e5e582b6bebe08
manu3618 <manu+github@hbrt.eu>
2024-05-08 19:53:42 +0200

    WIP on latex compilation

--- src/lib.rs
+++ src/lib.rs
@@ -186,5 +186,4 @@ mod tests {
         eprintln!("{result}");
         assert!(false);
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
+use std::fs;
 use std::path::Path;
+// use tectonic; // TODO use an optional dependency
 
 #[derive(Parser, Debug)]
 #[command(author, about)]
@@ -11,9 +13,16 @@ struct Args {
 fn main() {
     let args = Args::parse();
     let path = Path::new(&args.input);
-    let outname = path.with_extension("tex");
+    let out_tex = path.with_extension("tex");
     let in_ext = path.extension().unwrap();
     dbg!(&path);
     dbg!(&outname);
     dbg!(&in_ext);
+    let tex_data: String = todo!();
+    fs::write(path.with_extension("tex"), tex_data);
+
+    // TODO: use an optional dependency
+    // let pdf_data: Vec<u8> = tectonic::latex_to_pdf(tex_data).expect("processing failed");
+    // println!("Output PDF size is {} bytes", pdf_data.len());
+    // fs::write(path.with_extension("pdf"), pdf_data);
 }
