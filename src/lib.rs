60ac9fadda4bc116edce44e79c866269bf7e68ee
manu3618 <manu+github@hbrt.eu>
2024-05-08 19:53:42 +0200

    Add personal infos

--- src/lib.rs
+++ src/lib.rs
@@ -34,10 +34,30 @@ struct EntryDescription {
     other: Vec<String>,
 }
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Curriculum {
+    #[serde(rename = "personal data")]
+    personal_data: PersonalData,
     education: Vec<CVEntry>,
     experiences: Vec<EntryDescription>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct PersonalData {
+    name: String,
+    title: Option<String>,
+    #[serde(default)]
+    mobile: Vec<String>,
+    #[serde(default)]
+    email: Vec<String>,
+    github: Option<String>,
+    gitlab: Option<String>,
+    twitter: Option<String>,
+    linkedin: Option<String>,
+
+    #[serde(default)]
+    /// [(name, url), ]
+    webpage: Vec<(String, String)>,
 }
 
 #[derive(Debug)]
@@ -69,6 +89,45 @@ impl List {
     }
 }
 
+impl PersonalData {
+    fn to_latex(&self) -> String {
+        let mut lines = Vec::new();
+        lines.push("% personal data".into());
+        let names = &self.name.split(' ').collect::<Vec<_>>();
+        let first_name = names[0];
+        lines.push(format!("\\firstname{{\\LARGE {first_name}}}"));
+        if let Some(last_name) = names.get(1) {
+            lines.push(format!("\\familyname{{\\LARGE {last_name}}}"));
+        }
+        if let Some(title) = &self.title {
+            lines.push(format!("\\title{{{}}}", title));
+        }
+        for t in &self.mobile {
+            lines.push(format!("\\mobile{{{}}}", t));
+        }
+        for e in &self.email {
+            lines.push(format!("\\email{{{}}}", e));
+        }
+        // socials
+        if let Some(e) = &self.github {
+            lines.push(format!("\\social[github]{{{e}}}"))
+        }
+        if let Some(e) = &self.gitlab {
+            lines.push(format!("\\social[gitlab]{{{e}}}"))
+        }
+        if let Some(e) = &self.linkedin {
+            lines.push(format!("\\social[linkedin]{{{e}}}"))
+        }
+        if let Some(e) = &self.twitter {
+            lines.push(format!("\\social[twitter]{{{e}}}"))
+        }
+        for (n, u) in &self.webpage {
+            lines.push(format!("\\extrainfo{{\\homepagesymbol {n} \\url{{{u}}}}}"));
+        }
+        lines.join("\n")
+    }
+}
+
 mod tests {
     use super::*;
 
@@ -83,4 +142,49 @@ mod tests {
         "#;
         let entry: CVEntry = serde_json::from_str(&entry).unwrap();
     }
+
+    #[test]
+    fn read_personal_data() {
+        let data = r#"
+        {
+            "name": "Jessica",
+            "title": "Environmental manager",
+            "mobile": ["+32 56 19 01"]
+        }"#;
+        let personal_data: PersonalData = serde_json::from_str(&data).unwrap();
+    }
+
+    #[test]
+    fn write_incomplete_personal_data() {
+        let data = r#"
+        {
+            "name": "Jessica Meyer",
+            "title": "Environmental manager",
+            "mobile": ["+32 56 19 01", "+32 56 19 04"]
+        }"#;
+        let personal_data: PersonalData = serde_json::from_str(&data).unwrap();
+        let result = personal_data.to_latex();
+
+        eprintln!("{result}");
+        assert!(false);
+    }
+
+    #[test]
+    fn write_personal_data() {
+        let data = r#"
+        {
+            "name": "Jessica Meyer",
+            "title": "Environmental manager",
+            "mobile": ["+32 56 19 01", "+32 56 19 04"],
+            "email": ["nom@example.com", "nom@example.org"],
+            "github": "example",
+            "webpage": [["example", "www.example.com"]]
+        }"#;
+        let personal_data: PersonalData = serde_json::from_str(&data).unwrap();
+        let result = personal_data.to_latex();
+
+        eprintln!("{result}");
+        assert!(false);
+    }
+
 }
