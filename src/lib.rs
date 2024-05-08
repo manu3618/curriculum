f9bcb8850471a0f6bee698b375d8b72108e2f394
manu3618 <manu+github@hbrt.eu>
2024-05-08 19:53:43 +0200

    Add CVEntry skills extractor

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
+use anyhow::Result;
+use chrono::{DateTime, Duration, TimeZone, Utc};
 use serde::{Deserialize, Serialize};
-use time::Date;
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
 
-#[derive(Serialize, Deserialize, Debug)]
+static DATA_DIR: &str = "data";
+
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct CVEntry {
-    // beginning: Date,
-    // end: Option<Date>,
+    #[serde(default)]
+    #[serde(with = "cv_date")]
+    beginning: Option<DateTime<Utc>>,
+    #[serde(default)]
+    #[serde(with = "cv_date")]
+    end: Option<DateTime<Utc>>,
     /// degree or title
     #[serde(default)]
     degree: String,
@@ -19,30 +29,37 @@ struct CVEntry {
     description: Option<EntryDescription>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct EntryDescription {
+    #[serde(default)]
     context: String,
     /// technologies
     /// programming language
+    #[serde(default)]
     programming: Vec<String>,
     /// version control
+    #[serde(default)]
     version: Vec<String>,
+    #[serde(default)]
     database: Vec<String>,
+    #[serde(default)]
     cloud: Vec<String>,
     /// CI/CD
+    #[serde(default)]
     ci: Vec<String>,
+    #[serde(default)]
     other: Vec<String>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Curriculum {
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Curriculum {
     #[serde(rename = "personal data")]
     personal_data: PersonalData,
     education: Vec<CVEntry>,
     experiences: Vec<EntryDescription>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct PersonalData {
     name: String,
     title: Option<String>,
@@ -71,14 +88,43 @@ impl CVEntry {
     fn to_latex(&self) -> String {
         todo!()
     }
+
+    /// get skills
+    /// {category:Vec<skills>}
+    fn extract_skills(&self) -> HashMap<&str, Vec<String>> {
+        let mut skills = HashMap::new();
+        if let Some(desc) = &self.description {
+            skills.insert("programming languages", desc.programming.clone());
+            skills.insert("version control", desc.version.clone());
+            skills.insert("databases", desc.database.clone());
+            skills.insert("cloud computing", desc.cloud.clone());
+            skills.insert("CI/CD", desc.ci.clone());
+            skills.insert("other", desc.other.clone());
+        }
+        skills.retain(|_, v| !&v.is_empty());
+        skills
+    }
+
+    /// return duration of this entry
+    fn duration(&self) -> Option<Duration> {
+        if let Some(b) = &self.beginning {
+            if let Some(e) = &self.end {
+                Some(*e - b)
+            } else {
+                Some(Utc::now() - b)
+            }
+        } else {
+            None
+        }
+    }
 }
 
 impl List {
     fn to_latex(&self) -> String {
         format!(
             r#"\begin{{itemize}}
-        {}
-        \end{{itemize}}"#,
+                {}
+            \end{{itemize}}"#,
             &self
                 .0
                 .iter()
@@ -86,6 +132,12 @@ impl List {
                 .collect::<Vec<_>>()
                 .join("\n")
         )
+    }
+}
+
+impl EntryDescription {
+    fn to_latex(&self) -> String {
+        todo!()
     }
 }
 
@@ -128,6 +180,65 @@ impl PersonalData {
     }
 }
 
+impl Curriculum {
+    /// Generate the LaTeX corresponding to the whole document
+    pub fn to_latex(&self) -> Result<String> {
+        let mut output = Vec::new();
+        let preamb = fs::read(Path::new(DATA_DIR).join("preambule.tex"))?;
+        output.push(String::from_utf8(preamb)?);
+
+        output.push("\\end{document}".into());
+        Ok(output.join("\n"))
+    }
+    /// Get skills from entries
+    /// {category: {skill: duration}}
+    fn get_skills(&self) -> HashMap<String, HashMap<String, String>> {
+        todo!()
+    }
+}
+
+mod cv_date {
+    use chrono::{DateTime, TimeZone, Utc};
+    use serde::{self, Deserialize, Deserializer, Serializer};
+    const FORMAT: &'static str = "%Y-%m";
+
+    pub fn serialize<S>(date: &Option<DateTime<Utc>>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if let Some(date) = date {
+            let s = format!("{}", date.format(FORMAT));
+            serializer.serialize_str(&s)
+        } else {
+            serializer.serialize_none()
+        }
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s: Option<String> = Option::deserialize(deserializer)?;
+        if let Some(s) = s {
+            let date: Vec<_> = s.split("-").collect();
+            let date: DateTime<Utc> = Utc
+                .with_ymd_and_hms(
+                    str::parse(date[0]).unwrap(),
+                    str::parse(date[1]).unwrap(),
+                    1,
+                    0,
+                    0,
+                    0,
+                )
+                .unwrap();
+            Ok(Some(date))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -164,9 +275,6 @@ mod tests {
         }"#;
         let personal_data: PersonalData = serde_json::from_str(&data).unwrap();
         let result = personal_data.to_latex();
-
-        eprintln!("{result}");
-        assert!(false);
     }
 
     #[test]
@@ -182,8 +290,35 @@ mod tests {
         }"#;
         let personal_data: PersonalData = serde_json::from_str(&data).unwrap();
         let result = personal_data.to_latex();
+    }
 
-        eprintln!("{result}");
-        assert!(false);
+    #[test]
+    fn decription_tex() {
+        let data = r#"
+        {
+            "context": "some super context",
+            "ci": ["git", "gitlab"]
+        }
+        "#;
+        let entry: EntryDescription = serde_json::from_str(&data).unwrap();
+        let result = entry.to_latex();
+    }
+
+    #[test]
+    fn entry_extract_skills() {
+        let data = r#"
+        {
+            "beginning": "2023-11",
+            "city": "Brussels",
+            "description":
+                 {
+                    "context": "some super context",
+                    "ci": ["git", "gitlab"]
+                }
+        }
+        "#;
+        let entry: CVEntry = serde_json::from_str(&data).unwrap();
+        let skills = entry.extract_skills();
+        assert!(skills.keys().collect::<Vec<_>>().contains(&&"CI/CD"));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use clap::Parser;
+use curriculum::Curriculum;
 use std::fs;
 use std::path::Path;
 // use tectonic; // TODO use an optional dependency
@@ -10,19 +12,22 @@ struct Args {
     input: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
     let path = Path::new(&args.input);
-    let out_tex = path.with_extension("tex");
     let in_ext = path.extension().unwrap();
     dbg!(&path);
-    dbg!(&outname);
     dbg!(&in_ext);
-    let tex_data: String = todo!();
-    fs::write(path.with_extension("tex"), tex_data);
+
+    let cv = Curriculum::default(); // TODO: from_json
+    let tex_data = cv.to_latex()?;
+    let tex_path = path.with_extension("tex");
+    println!("wrinting {}", tex_path.display());
+    let _ = fs::write(tex_path, tex_data);
 
     // TODO: use an optional dependency
     // let pdf_data: Vec<u8> = tectonic::latex_to_pdf(tex_data).expect("processing failed");
     // println!("Output PDF size is {} bytes", pdf_data.len());
     // fs::write(path.with_extension("pdf"), pdf_data);
+    Ok(())
 }
